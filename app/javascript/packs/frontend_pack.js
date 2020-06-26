af5560a0d7065047f8f82f78cf1cd54117d172d4
Jorge Muñoz <sagerxl@gmail.com>
2020-06-25 20:18:59 -0500

    se añadió yield styles y scripts

--- app/javascript/packs/frontend_pack.js
+++ app/javascript/packs/frontend_pack.js
@@ -1,10 +1,2 @@
-import 'bootstrap';
-import 'jquery';
-import 'jquery-nice-select';
-
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
 
 require('./custom');
