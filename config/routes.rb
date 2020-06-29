222122e2aa46b6c57c082a2f98a6191f57646a41
Jordy Peralta <jgpo1@hotmail.com>
2020-06-28 23:30:30 -0500

    corrigiendo ruta de registrate

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :marcas
   get 'logout' => 'sessions#logout'
   get 'login' => "sessions#login"
+  get 'register' => "page#registrate"
   post 'login' => "sessions#validar"
   get 'recover' => "sessions#recover"
   get 'contact' => "page#contact"
@@ -25,7 +26,7 @@ Rails.application.routes.draw do
   get 'equipos/huaweip40'
   get 'equipos/samsumga51'
   get 'page/login'
-  get 'page/registrate'
+#  get 'page/registrate'
   post 'page/para_registro'
   
   get 'page/planes'
