7923b9f03abb06f323e87bb940fb51f3b47adb1b
Cristhian05T <66043380+Cristhian05T@users.noreply.github.com>
2020-06-28 20:11:40 -0500

    Avance
    Se genero la pagina about us

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -3,5 +3,5 @@ class ShopController < ApplicationController
 
     def index
     end
-
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
 
   get 'shop' => 'shop#index'
   get 'shop/index'
-
+  get 'home/aboutus'
   get 'equipos/motorolla'
   get 'equipos/samsungs10'
   get 'equipos/huaweip40'
   get 'equipos/samsumga51'
   get 'page/login'
-  
   get 'page/registrate'
   post 'page/para_registro'
   
